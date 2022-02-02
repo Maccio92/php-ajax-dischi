32e48ea2ee449a75c1b6515fcad8070b8e6b15e1
Maccio92 <valerio362@gmail.com>
2022-02-02 15:01:18 +0100

    add header & footer

---
+++ partials/footer.php
@@ -0,0 +1,3 @@
+<div class="container-fluid footer-container">
+    <h3>Copyright 2022</h3>
+</div>
---
+++ partials/header.php
@@ -0,0 +1,3 @@
+<div class="container-fluid px-5 header-container">
+    <img class="logo" src="https://upload.wikimedia.org/wikipedia/commons/thumb/1/19/Spotify_logo_without_text.svg/2048px-Spotify_logo_without_text.svg.png" alt="">
+</div>
